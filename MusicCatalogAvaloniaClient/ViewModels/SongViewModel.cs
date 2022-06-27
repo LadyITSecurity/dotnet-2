00553d4c762c36bb3a93175ce3ac0151faa2d117
Arisa <uliannaobr@gmail.com>
2022-06-27 12:48:15 +0400

    add SongViewModel in MusicCatalogAvaloniaClient

--- MusicCatalogAvaloniaClient/ViewModels/SongViewModel.cs
+++ MusicCatalogAvaloniaClient/ViewModels/SongViewModel.cs
@@ -1,22 +1,61 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive;
 using System.Text;
 using System.Threading.Tasks;
 
 using MusicCatalogServer.Api;
 
+using ReactiveUI;
+
 namespace MusicCatalogAvaloniaClient.ViewModels
 {
-    public class SongViewModel
+    public class SongViewModel : ReactiveObject
     {
-
-        public Song ThisSong { get; set; } = new();
+        public MainViewModel _MainViewModel { get; set; }
+        public Song SongProto { get; set; } = new();
         public int ID { get; set; }
+        public string Title { get; set; }
         public List<string> Singers { get; set; } = new();
         public List<string> Genres { get; set; } = new();
         public int DurationSecs { get; set; }
+        public ReactiveCommand<Unit, Unit> OkCommand { get; }
+        public ReactiveCommand<Unit, Unit> CancelCommand { get; }
 
+        public SongViewModel(MainViewModel mainViewModel, Song songProto)
+        {
+            _MainViewModel = mainViewModel;
+            SongProto = songProto;
+            ID = songProto.Id;
+            Title = songProto.Title;
+            DurationSecs = songProto.DurationSecs;
+            foreach (var singer in songProto.Singers)
+                Singers.Add(singer);
+            foreach (var genre in songProto.Genres)
+                Genres.Add(genre);
+        }
+        public SongViewModel(MainViewModel mainViewModel)
+        {
+            OkCommand = ReactiveCommand.Create(Add);
+            CancelCommand = ReactiveCommand.Create(Cancel);
+        }
+
+
+
+        public void Add()
+        {
+            if (Title == string.Empty)
+            {
+                //
+            }
+
+        }
+
+        private void Cancel()
+        {
+            throw new NotImplementedException();
+        }
 
     }
 }
